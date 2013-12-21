2c4554abf99cc617eae95aaab58b71d6023ea19e
vorou <vorou@skbkontur.ru>
2013-12-21 13:56:30 +0600

    fix build script & psake

--- default.ps1
+++ default.ps1
@@ -1,15 +1,17 @@
 Properties {
     $solution_dir = ".\Src"
-    $verbosity = "minimal"
     $test_runner = "$solution_dir\packages\Fixie.0.0.1.120\lib\net45\Fixie.Console.exe"
+    $verbosity = "minimal"
 }
 
-Task default -Depends Compile
+Task default -Depends build
 
-Task Compile {
-    Exec { msbuild /verbosity:$verbosity "$solution_dir\playNET.sln" }
+Task build -Depends compile, tests
+
+Task compile {
+    exec { msbuild /v:$verbosity "$solution_dir\playNET.sln" }
 }
 
-Task RunTests -Depends Compile {
-    Call "$test_runner" "$solution_dir\playNET.Tests\bin\Debug\playNET.Tests.dll"
+Task tests -Depends compile {
+    Exec { & "$test_runner" "$solution_dir\playNET.Tests\bin\Debug\playNET.Tests.dll" }
 }
