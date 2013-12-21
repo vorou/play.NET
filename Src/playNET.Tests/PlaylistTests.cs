4ba147c3a9a8eb75ab9c8b9c1374bf62d916f82c
vorou <vorou@skbkontur.ru>
2013-12-21 18:20:42 +0600

    Playlist

---
+++ Src/playNET.Tests/PlaylistTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Shouldly;
+
+namespace playNET.Tests
+{
+    public class PlaylistTests : IDisposable
+    {
+        private readonly string targetDirectory;
+
+        public PlaylistTests()
+        {
+            targetDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(targetDirectory);
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(targetDirectory))
+                Directory.Delete(targetDirectory, true);
+        }
+
+        private static void CreateEmptyFile(string path)
+        {
+            File.WriteAllBytes(path, new byte[0]);
+        }
+
+        public void Playlist_DirectoryEmpty_ReturnsNoTracks()
+        {
+            var sut = new Playlist(targetDirectory);
+
+            var actual = sut.GetTracks();
+
+            actual.ShouldBeEmpty();
+        }
+
+        public void Playlist_OneMp3File_ReturnsIt()
+        {
+            var pathToMp3 = Path.Combine(targetDirectory, Path.ChangeExtension(Path.GetRandomFileName(), ".mp3"));
+            CreateEmptyFile(pathToMp3);
+            var sut = new Playlist(targetDirectory);
+
+            var actual = sut.GetTracks();
+
+            actual.ShouldContain(pathToMp3);
+        }
+
+        public void Playlist_FileWithUnknownExtension_IgnoresIt()
+        {
+            var pathToUnknownFile = Path.Combine(targetDirectory, Path.GetRandomFileName());
+            CreateEmptyFile(pathToUnknownFile);
+            var sut = new Playlist(targetDirectory);
+
+            var actual = sut.GetTracks();
+
+            actual.ShouldNotContain(pathToUnknownFile);
+        }
+    }
+}
---
+++ Src/playNET/Playlist.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace playNET
+{
+    public class Playlist : IPlaylist
+    {
+        private readonly string directory;
+
+        public Playlist(string directory)
+        {
+            this.directory = directory;
+        }
+
+        public IEnumerable<string> GetTracks()
+        {
+            return Directory.GetFiles(directory, "*.mp3");
+        }
+    }
+}
