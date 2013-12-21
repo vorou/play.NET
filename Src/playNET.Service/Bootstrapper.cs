30db7f71ae98a7a437c7fe5b3e1073444ff08306
vorou <vorou@skbkontur.ru>
2013-12-21 19:42:46 +0600

    self-host for service & bootstrapper

---
+++ Src/playNET.Service/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using Nancy.TinyIoc;
+
+namespace playNET.Service
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            container.Register(Singer.Instance);
+            container.Register<IPlaylist>(new Playlist(@"C:\playNET"));
+        }
+    }
+}
--- Src/playNET.Service/Program.cs
+++ Src/playNET.Service/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace playNET.Service
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace playNET.Service
 {
     public class Program
     {
         private static void Main()
         {
+            var baseUri = new Uri("http://localhost:666");
+            using (var host = new NancyHost(baseUri))
+            {
+                host.Start();
+                Console.Out.WriteLine("playNET is ready at {0}, press any key to stop...", baseUri);
+                Console.ReadLine();
+            }
         }
     }
 }
