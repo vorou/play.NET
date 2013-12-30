35458f41233fb8e553b9690475106e77be3e245a
vorou <vorou@skbkontur.ru>
2013-12-31 00:10:41 +0600

    test convention uses af to generate test data

--- Src/playNET.Tests/Helpers/TestConvention.cs
+++ Src/playNET.Tests/Helpers/TestConvention.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Fixie;
 using Fixie.Conventions;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoFakeItEasy;
+using Ploeh.AutoFixture.Kernel;
+using ReflectionExtensions = Fixie.ReflectionExtensions;
 
 namespace playNET.Tests.Helpers
 {
     public class TestConvention : Convention
     {
+        private readonly ISpecimenContext specimen = new SpecimenContext(new Fixture().Customize(new AutoFakeItEasyCustomization()));
+
         public TestConvention()
         {
             Classes.NameEndsWith("Tests");
-            Methods.Where(method => method.IsVoid());
-            Parameters(FromInputAttributes);
+            Methods.Where(method => ReflectionExtensions.IsVoid(method));
+            Parameters(ForMethod);
         }
 
-        private static IEnumerable<object[]> FromInputAttributes(MethodInfo method)
+        private IEnumerable<object[]> ForMethod(MethodInfo method)
         {
-            return method.GetCustomAttributes<InputAttribute>(true).Select(input => input.Parameters);
+            var inputs = method.GetCustomAttributes<InputAttribute>(true);
+            if (inputs.Any())
+                return inputs.Select(input => input.Parameters);
+
+            return AutofixtureParametersFor(method);
+        }
+
+        private IEnumerable<object[]> AutofixtureParametersFor(MethodInfo method)
+        {
+            var parameterTypes = method.GetParameters().Select(x => x.ParameterType);
+            var parameterValues = parameterTypes.Select(t => specimen.Resolve(t)).ToArray();
+            return new[] {parameterValues};
         }
     }
 }
