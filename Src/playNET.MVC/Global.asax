7f1a91211c8fd0758686d14de87393c51a561752
vorou <vorou@skbkontur.ru>
2014-01-02 15:51:56 +0600

    add empty mvc app

---
+++ Src/playNET.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="playNET.MVC.MvcApplication" Language="C#" %>
---
+++ Src/playNET.MVC/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace playNET.MVC
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RegisterRoutes();
+        }
+
+        private static void RegisterRoutes()
+        {
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteTable.Routes.MapRoute(name: "Default",
+                                       url: "{controller}/{action}/{id}",
+                                       defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional});
+        }
+    }
+}
