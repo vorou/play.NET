f65b19bd4f54bf70a351a136c9759136e14361d1
vorou <vorou@skbkontur.ru>
2014-01-05 03:37:05 +0600

    EXTRA GRANULARITY!!!

--- default.ps1
+++ default.ps1
@@ -1,20 +1,25 @@
 Properties {
     $verbosity = "minimal"
     $sln_dir = ".\Src"
+    $sln = "$sln_dir\playNET.sln"
     $msbuild = "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\MSBuild.exe"
     $test_runner = "$sln_dir\packages\Fixie.0.0.1.120\lib\net45\Fixie.Console.exe"
-    $sln = "$sln_dir\playNET.sln"
+    $test_proj = "$sln_dir\playNET.Tests\playNET.Tests.csproj" 
     $test_dll = "$sln_dir\playNET.Tests\bin\Debug\playNET.Tests.dll" 
 }
 
 Task default -Depends build
 
-Task build -Depends compile, tests
+Task build -Depends compile_sln, run_tests
 
-Task compile {
+Task compile_sln {
     Exec { & $msbuild /v:$verbosity $sln }
 }
 
-Task tests -Depends compile {
+Task compile_tests {
+    Exec { & $msbuild /v:$verbosity $test_proj }
+}
+
+Task run_tests -Depends compile_tests {
     Exec { & $test_runner $test_dll }
 }
