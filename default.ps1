5cce9ede438e9cd2b83b57d4baf42774930ecf0b
vorou <vorou@skbkontur.ru>
2014-01-05 02:20:41 +0600

    clean up build script

--- default.ps1
+++ default.ps1
@@ -1,8 +1,10 @@
 Properties {
-    $solution_dir = ".\Src"
-    $test_runner = "$solution_dir\packages\Fixie.0.0.1.120\lib\net45\Fixie.Console.exe"
-    $msbuild = "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\MSBuild.exe"
     $verbosity = "minimal"
+    $sln_dir = ".\Src"
+    $msbuild = "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\MSBuild.exe"
+    $test_runner = "$sln_dir\packages\Fixie.0.0.1.120\lib\net45\Fixie.Console.exe"
+    $sln = "$sln_dir\playNET.sln"
+    $test_dll = "$sln_dir\playNET.Tests\bin\Debug\playNET.Tests.dll" 
 }
 
 Task default -Depends build
@@ -10,9 +12,9 @@ Task default -Depends build
 Task build -Depends compile, tests
 
 Task compile {
-    Exec { & "$msbuild" /v:$verbosity "$solution_dir\playNET.sln" }
+    Exec { & $msbuild /v:$verbosity $sln }
 }
 
 Task tests -Depends compile {
-    Exec { & "$test_runner" "$solution_dir\playNET.Tests\bin\Debug\playNET.Tests.dll" }
+    Exec { & $test_runner $test_dll }
 }
